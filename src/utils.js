fcd68a80131b83b562ae7abcf42860ff8a4c397e
Christopher Pappas <damassi.pappas@gmail.com>
2018-04-23 14:02:11 -0700

    Apply prettier

--- src/utils.js
+++ src/utils.js
@@ -1,17 +1,17 @@
 import { isFunction, isString } from 'lodash'
 
-export function isTemplate (template) {
+export function isTemplate(template) {
   if (!isString(template)) {
     return false
   }
 
   const BLACKLIST = ['.js', '.jsx', '.ts', '.tsx']
 
-  const found = BLACKLIST.some((extension) => template.includes(extension))
+  const found = BLACKLIST.some(extension => template.includes(extension))
   return !found
 }
 
-export function isComponent (Component) {
+export function isComponent(Component) {
   if (isFunction(Component)) {
     return true
   } else {
